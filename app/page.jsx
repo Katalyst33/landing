15a317a7734c8c5084efcf1ae289fbbea8874554
katalyst33 <>
2024-02-27 13:28:31 +0000

    Revert "Merge branch 'main' into typewritter"
    This reverts commit 94e5c3e199626f73a9efa64e3651a84bed3763cb, reversing
    changes made to a8eb96d67934b906ebc832b3d095a6ecedb99ebb.

--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,10 @@
 import Image from "next/image";
 import ProjectComponent from "./components/ProjectComponent";
 import { projectList, services, tools } from "./data/content";
-import HomeServices from "./components/HomeServices.jsx";
-import TestimonialFeature from "./components/TestimonialFeature.jsx";
-import BeforeFooter from "./components/BeforeFooter.jsx";
-import { TypewriterEffect } from "./components/typewriter-effect";
-import HomeHero from "./components/HomeHero.jsx";
-import HomeFeature from "./components/HomeFeature.jsx";
+import HomeHero from "./components/HomeHero.jsx"
+import HomeFeature from "./components/HomeFeature.jsx"
+import HomeServices from "./components/HomeServices.jsx"
+import {TypewriterEffect} from "./components/typewriter-effect";
 
 export default function Home() {
   const serviceImage = [
@@ -22,41 +20,55 @@ export default function Home() {
         "https://res.cloudinary.com/dqwfjxn8g/image/upload/v1697268006/rear-view-programmer-working-all-night-long_x0yjx4.jpg",
     },
   ];
-  const words = [
-    {
-      text: "Build",
-    },
-    {
-      text: "awesome",
-    },
-    {
-      text: "apps",
-    },
-    {
-      text: "with",
-    },
-    {
-      text: "Aceternity.",
-      className: "text-blue-500 dark:text-blue-500",
-    },
-  ];
+    const words = [
+        {
+            text: "Build",
+        },
+        {
+            text: "awesome",
+        },
+        {
+            text: "apps",
+        },
+        {
+            text: "with",
+        },
+        {
+            text: "Aceternity.",
+            className: "text-blue-500 dark:text-blue-500",
+        },
+    ];
 
   return (
     <main className=" ">
-      <HomeHero />
+      <div className={`bg-black p-10`}>
+         <TypewriterEffect words={words}/>
+      </div>
+      <HomeHero/>
 
-      <HomeFeature />
+      <HomeFeature/>
+
 
       <section className="py-40 w-11/12 mx-auto">
         <div className="mx-auto ">
-          <HomeServices />
-          <TestimonialFeature />
-          <div className={`bg-black p-10`}>
-            <TypewriterEffect words={words} />
-          </div>
+        <HomeServices/>
         </div>
-
-        <BeforeFooter />
+      </section>
+      <section className="w-11/12 mx-auto rounded-3xl bg-black text-white">
+        <div className="w-4/6 mx-auto py-20">
+          <p className="text-4xl">Tell us about your project</p>
+          <button className="px-3 py-2 my-10 text-black bg-white rounded-lg">
+            Leave us a message
+          </button>
+          <hr className="text-gray-500 mb-10" />
+          <p className="font-semibold mb-2">Katalyst Technologies</p>
+          <p>
+            At Katalyst Technologies we don't just design digital products, we
+            design experiences for people like you, You need the right minds
+            that can interpret your ideas and make it a reality we are glad you
+            found us.
+          </p>
+        </div>
       </section>
     </main>
   );
