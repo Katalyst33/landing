190822c72a301c95e4344dc3b13179ee8f732bb8
katalyst33 <>
2024-03-08 12:01:27 +0000

    fixed team page

--- app/team/[slug]/page.jsx
+++ app/team/[slug]/page.jsx
@@ -1,10 +1,7 @@
-
-
 async function getData(id) {
   const res = await fetch(`${process.env.serverUrl}/team/${id}`, {
     // cache: 'force-cache',
   });
-
 
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
@@ -18,8 +15,7 @@ export default async function TeamPost({ params }) {
 
   const data = await getData(slug);
 
-  const { name, position, skills,education, summary,experience } = data;
-
+  const { name, position, skills, education, summary, experience } = data;
 
   return (
     <main>
@@ -40,64 +36,70 @@ export default async function TeamPost({ params }) {
             </div>
           </div>
 
+          <div className="">
+            <div className="relative lg:col-start-2 lg:row-start-1">
+              <div className="relative mx-auto max-w-prose text-base lg:max-w-none">
+                <div className="prose prose-indigo mx-auto mt-5 text-gray-500 lg:max-w-none">
+                  {experience && (
+                    <>
+                      <h3 className="text-3xl font-bold tracking-tight text-indigo-600">
+                        Experience
+                      </h3>
+                      {experience.map((exp, index) => (
+                        <div key={index} className="mt-4">
+                          <h4 className="text-xl font-bold">{exp.title}</h4>
+                          <p className="text-lg font-medium text-gray-600">
+                            {exp.company} - {exp.location}
+                          </p>
+                          <p className="text-lg font-medium text-gray-600">
+                            {exp.date}
+                          </p>
+                          <p className="mt-2 text-lg text-gray-500">
+                            {exp.description}
+                          </p>
+                        </div>
+                      ))}
+                    </>
+                  )}
 
-        <div className="">
-          <div className="relative lg:col-start-2 lg:row-start-1">
-            <div className="relative mx-auto max-w-prose text-base lg:max-w-none">
-              <div className="prose prose-indigo mx-auto mt-5 text-gray-500 lg:max-w-none">
-                <h3 className="text-3xl font-bold tracking-tight text-indigo-600">
-                  Experience
-                </h3>
-                {experience.map((exp, index) => (
-                  <div key={index} className="mt-4">
-                    <h4 className="text-xl font-bold">{exp.title}</h4>
-                    <p className="text-lg font-medium text-gray-600">
-                      {exp.company} - {exp.location}
-                    </p>
-                    <p className="text-lg font-medium text-gray-600">
-                      {exp.date}
-                    </p>
-                    <p className="mt-2 text-lg text-gray-500">
-                      {exp.description}
-                    </p>
-                  </div>
-                ))}
-                <h3 className="text-3xl font-bold tracking-tight text-indigo-600 mt-8">
-                  Education
-                </h3>
+                  {education && (
+                    <>
+                      <h3 className="text-3xl font-bold tracking-tight text-indigo-600 mt-8">
+                        Education
+                      </h3>
 
+                      {education.map((edu, index) => (
+                        <div key={index} className="mt-4">
+                          <h4 className="text-xl font-bold">{edu.degree}</h4>
+                          <p className="text-lg font-medium text-gray-600">
+                            {edu.school} - {edu.location}
+                          </p>
+                          <p className="text-lg font-medium text-gray-600">
+                            {edu.date}
+                          </p>
+                        </div>
+                      ))}
+                    </>
+                  )}
 
-
-                {education.map((edu, index) => (
-                  <div key={index} className="mt-4">
-                    <h4 className="text-xl font-bold">{edu.degree}</h4>
-                    <p className="text-lg font-medium text-gray-600">
-                      {edu.school} - {edu.location}
-                    </p>
-                    <p className="text-lg font-medium text-gray-600">
-                      {edu.date}
-                    </p>
-                  </div>
-                ))}
-
-
-                <h3 className="text-3xl font-bold tracking-tight text-indigo-600 mt-8">
-                  Skills
-                </h3>
-                <ul className="list-disc list-inside mt-4">
-
-                  {skills.map((skill, index) => (
-                    <li key={index} className="text-lg text-gray-500">
-                      {skill}
-                    </li>
-                  ))}
-
-                </ul>
+                  {skills && (
+                    <>
+                      <h3 className="text-3xl font-bold tracking-tight text-indigo-600 mt-8">
+                        Skills
+                      </h3>
+                      <ul className="list-disc list-inside mt-4">
+                        {skills.map((skill, index) => (
+                          <li key={index} className="text-lg text-gray-500">
+                            {skill}
+                          </li>
+                        ))}
+                      </ul>
+                    </>
+                  )}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-
         </div>
       </h1>
     </main>
