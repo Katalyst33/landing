44aebae4e1db18703584a4b004af3589268cedd4
katalyst33 <>
2024-03-08 10:55:36 +0000

    integrated team page

--- app/team/[slug]/page.jsx
+++ app/team/[slug]/page.jsx
@@ -1,31 +1,45 @@
-'use client';
 
-import { everyOne } from '../../data/team';
-export default function TeamPost({ params }) {
-  let selected = {};
 
-  for (const item of everyOne) {
-    if (item.slug === params.slug) {
-      selected = item;
-    }
+async function getData(id) {
+  const res = await fetch(`${process.env.serverUrl}/team/${id}`, {
+    // cache: 'force-cache',
+  });
+
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
   }
+  return res.json();
+}
 
-  const { name, position, summary } = selected;
+export default async function TeamPost({ params }) {
+  const slug = params.slug;
+
+  const data = await getData(slug);
+
+  const { name, position, summary } = data;
 
   return (
-    <div className="overflow-hidden bg-white">
-      <div className="relative mx-auto max-w-7xl px-6 py-16 lg:px-8">
-        <div className="mx-auto max-w-prose text-base lg:max-w-none">
-          <h2 className="text-3xl font-bold tracking-tight text-indigo-600">
-            Professional Summary here
-          </h2>
-          <h3 className="mt-2 text-4xl font-bold leading-8 tracking-tight text-gray-900 sm:text-5xl">
-            {name}
-          </h3>
-          <p className="mt-2 text-xl font-medium text-gray-600">{position}</p>
-          <p className="mt-4 text-lg text-gray-500">{summary}</p>
+    <main>
+      <h1>
+        <div className="overflow-hidden bg-white">
+          <div className="relative mx-auto max-w-7xl px-6 py-16 lg:px-8">
+            <div className="mx-auto max-w-prose text-base lg:max-w-none">
+              <h2 className="text-3xl font-bold tracking-tight text-indigo-600">
+                Professional Summary here
+              </h2>
+              <h3 className="mt-2 text-4xl font-bold leading-8 tracking-tight text-gray-900 sm:text-5xl">
+                {name}
+              </h3>
+              <p className="mt-2 text-xl font-medium text-gray-600">
+                {position}
+              </p>
+              <p className="mt-4 text-lg text-gray-500">{summary}</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </h1>
+    </main>
   );
 }
