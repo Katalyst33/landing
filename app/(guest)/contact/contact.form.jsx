ebe30caba917105d9cc3c35229965825aea4d3f4
katalyst33 <>
2024-03-11 02:50:44 +0000

    end to end validation

--- app/(guest)/contact/contact.form.jsx
+++ app/(guest)/contact/contact.form.jsx
@@ -1,188 +1,184 @@
-import {Label} from '../../components/ui/label';
-import {Input} from '../../components/ui/input';
-import {Button} from '../../components/ui/button';
-import {useEffect, useState} from 'react';
+import { Label } from '../../components/ui/label';
+import { Input } from '../../components/ui/input';
+import { Button } from '../../components/ui/button';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import * as z from 'zod';
-import ErrorField from "../../components/ui/error-field";
-import {Textarea} from "../../components/ui/textarea";
+import ErrorField from '../../components/ui/error-field';
+import { Textarea } from '../../components/ui/textarea';
 
-export default function ContactForm({...props}) {
-    const [formData, setFormData] = useState({
-        email: '',
-        name: '',
-        message: '',
+export default function ContactForm({ ...props }) {
+  const [formData, setFormData] = useState({
+    email: '',
+    name: '',
+    message: '',
+  });
+
+  const [errors, setErrors] = useState({
+    email: '',
+    name: '',
+    message: '',
+  });
+
+  useEffect(() => {
+    setErrors({
+      email: props.errors?.email,
+      name: props.errors?.name,
+      message: props.errors?.message,
     });
+  }, [props.errors]);
 
-    const [errors, setErrors] = useState({
-        email: "",
-        name:"",
-        message: "",
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-    });
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
 
-    useEffect(() => {
-        setErrors({
-            email: props.errors?.email,
-            name: props.errors?.name,
-            message: props.errors?.message,
+    // Validate the changed field and update errors state
+    const updatedField = { [name]: value };
+    const fieldValidationSchema = schema.pick({ [name]: true }); // Assuming `schema` is your Zod schema
+
+    fieldValidationSchema.safeParseAsync(updatedField).then((result) => {
+      if (result.success) {
+        // If the field is valid, remove any existing error for this field
+        setErrors((prevErrors) => {
+          const newErrors = { ...prevErrors };
+          delete newErrors[name]; // Remove the error for this field
+          return newErrors;
         });
-    }, [props.errors]);
-
-    const handleChange = (e) => {
-        const {name, value} = e.target;
-
-        setFormData((prevState) => ({
-            ...prevState,
-            [name]: value,
+      } else {
+        // If the field is invalid, update the error message for this field
+        const errorMessage = result.error.errors[0]?.message || 'Invalid input';
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          [name]: errorMessage,
         }));
-
-        // Validate the changed field and update errors state
-        const updatedField = {[name]: value};
-        const fieldValidationSchema = schema.pick({[name]: true}); // Assuming `schema` is your Zod schema
-
-        fieldValidationSchema.safeParseAsync(updatedField).then((result) => {
-            if (result.success) {
-                // If the field is valid, remove any existing error for this field
-                setErrors((prevErrors) => {
-                    const newErrors = {...prevErrors};
-                    delete newErrors[name]; // Remove the error for this field
-                    return newErrors;
-                });
-            } else {
-                // If the field is invalid, update the error message for this field
-                const errorMessage = result.error.errors[0]?.message || "Invalid input";
-                setErrors((prevErrors) => ({
-                    ...prevErrors,
-                    [name]: errorMessage,
-                }));
-            }
-        });
-    };
-    const schema = z.object({
-        name: z.string().min(2, {
-            message: 'Name must be at least 2 characters long',
-        }),
-
-        email: z.string().email({
-            message: 'Invalid email address',
-        }),
-
-        message: z
-            .string()
-            .min(10, {
-                message: "Message must be at least 10 characters.",
-            })
-            .max(160, {
-                message: "Message must not be longer than 30 characters.",
-            }),
+      }
     });
+  };
+  const schema = z.object({
+    name: z.string().min(2, {
+      message: 'Name must be at least 2 characters long',
+    }),
 
-    function handleError() {
-        const result = schema.safeParse(formData);
+    email: z.string().email({
+      message: 'Invalid email address',
+    }),
 
-        if (!result.success) {
-            const formattedErrors = {};
-            result.error.errors.forEach((error) => {
-                const fieldName = error.path[0];
-                formattedErrors[fieldName] = error.message;
-            });
-            setErrors(formattedErrors);
+    message: z
+      .string()
+      .min(10, {
+        message: 'Message must be at least 10 characters.',
+      })
+      .max(160, {
+        message: 'Message must not be longer than 30 characters.',
+      }),
+  });
 
+  function handleError() {
+    const result = schema.safeParse(formData);
 
-            const firstError = Object.keys(formattedErrors)[0];
+    if (!result.success) {
+      const formattedErrors = {};
+      result.error.errors.forEach((error) => {
+        const fieldName = error.path[0];
+        formattedErrors[fieldName] = error.message;
+      });
+      setErrors(formattedErrors);
 
-            const element = document.getElementById('contact-' + firstError);
+      const firstError = Object.keys(formattedErrors)[0];
 
-            if (element) {
-                element.scrollIntoView({
-                    behavior: 'smooth',
-                    inline: 'start',
-                    block: 'center',
-                });
-            }
+      const element = document.getElementById('contact-' + firstError);
 
-            return;
-        } else {
-            setErrors({});
-            return true;
-        }
+      if (element) {
+        element.scrollIntoView({
+          behavior: 'smooth',
+          inline: 'start',
+          block: 'center',
+        });
+      }
+
+      return;
+    } else {
+      setErrors({});
+      return true;
     }
+  }
 
-    function handleSubmit(e) {
-        e.preventDefault();
+  function handleSubmit(e) {
+    e.preventDefault();
 
+    if (handleError()) props.handleSubmit(formData);
+  }
 
-        if (handleError())
-       props.handleSubmit(formData);
-    }
-
-    return (
-        <div className={`w-full`}>
-            <form onSubmit={handleSubmit}>
-                <div>
-
-                    <div>
-                        <Label htmlFor="email">Full Name</Label>
-                        <Input
-                            type="text"
-                            placeholder="What can we call you ?"
-                            id="conatct-name"
-                            name="name"
-                            value={formData.name}
-                            onChange={handleChange}
-                            className="w-full"
-                        />
-                        <ErrorField error={errors.name}/>
-                    </div>
-                    <div>
-                        <div>
-                            <Label htmlFor="email">Email</Label>
-                            <Input
-                                type="email"
-                                placeholder="Email Address"
-                                id="app-email"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleChange}
-                                className="w-full"
-                            />
-                            <ErrorField error={errors.email}/>
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                            <Label htmlFor="message">Message</Label>
-                            <Textarea
-                                name="message"
-
-                                value={formData.message}
-                                      onChange={handleChange} placeholder="Type your message here."/>
-                            <ErrorField error={errors.message}/>
-                        </div>
-                    </div>
-                </div>
-
-                <div>
-                    <div className="mt-8 flex justify-start">
-                        <Button
-                            variant="primary"
-                            size="lg"
-                            className="w-80 text-white "
-                            type="submit"
-                        >
-                            Submit Application
-                        </Button>
-                    </div>
-                </div>
-            </form>
+  return (
+    <div className={`w-full`}>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div>
+            <Label htmlFor="email">Full Name</Label>
+            <Input
+              type="text"
+              placeholder="What can we call you ?"
+              id="conatct-name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              className="w-full"
+            />
+            <ErrorField error={errors.name} />
+          </div>
+          <div>
+            <div>
+              <Label htmlFor="email">Email</Label>
+              <Input
+                type="email"
+                placeholder="Email Address"
+                id="app-email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="w-full"
+              />
+              <ErrorField error={errors.email} />
+            </div>
+          </div>
+          <div>
+            <div>
+              <Label htmlFor="message">Message</Label>
+              <Textarea
+                name="message"
+                value={formData.message}
+                onChange={handleChange}
+                placeholder="Type your message here."
+              />
+              <ErrorField error={errors.message} />
+            </div>
+          </div>
         </div>
-    );
+
+        <div>
+          <div className="mt-8 flex justify-start">
+            <Button
+              variant="primary"
+              size="lg"
+              className="w-80 text-white "
+              type="submit"
+            >
+              Submit Application
+            </Button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 ContactForm.propTypes = {
-    handleSubmit: PropTypes.func.isRequired,
-    className: PropTypes.string,
-    errors: PropTypes.object,
+  handleSubmit: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  errors: PropTypes.object,
 };
--- app/config/http.jsx
+++ app/config/http.jsx
@@ -1,18 +1,17 @@
 export async function $ClientFetch(url, options) {
-    try {
-        const response = await fetch(url, options);
-        const data = await response.json();
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
 
-        if (!response.ok) {
-            // Create an error with the response and the data for clients to handle
+    if (!response.ok) {
+      // Create an error with the response and the data for clients to handle
 
-            throw data;
-        }
-
-        return data; // Return data for successful responses
-
-    } catch (error) {
-        // This will handle network errors and the errors thrown above
-        throw error;
+      throw data;
     }
+
+    return data; // Return data for successful responses
+  } catch (error) {
+    // This will handle network errors and the errors thrown above
+    throw error;
+  }
 }
