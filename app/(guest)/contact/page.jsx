b685e4f17aa8344fe60fc4345596e75baa7bbf68
katalyst33 <>
2024-03-11 02:50:44 +0000

    fixed them toggler

--- app/(guest)/contact/page.jsx
+++ app/(guest)/contact/page.jsx
@@ -1,83 +1,76 @@
 'use client';
 import {
-    BsDribbble,
-    BsFacebook,
-    BsInstagram,
-    BsLinkedin,
+  BsDribbble,
+  BsFacebook,
+  BsInstagram,
+  BsLinkedin,
 } from 'react-icons/bs';
 
 import ContactForm from './contact.form';
-import {useState} from "react";
-import {$ClientFetch, $clientFetch, clientFetch, useClientFetch} from "../../config/http";
+import { useState } from 'react';
+import {
+  $ClientFetch,
+
+} from '../../config/http';
 
 export default function About() {
+  const [errors, setErrors] = useState({});
 
-    const [errors, setErrors] = useState({});
+  async function handleSubmit(formData) {
+    try {
+      const res = await $ClientFetch(`${process.env.apiUrl}/contact`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
 
-    async function handleSubmit(formData) {
-
-
-        try {
-            const res = await $ClientFetch(`${process.env.apiUrl}/contact`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(formData),
-            })
-
-
-            console.log(res, "error");
-
-        } catch (errors) {
-            setErrors(errors.errors);
-        }
-
-
+      console.log(res, 'error');
+    } catch (errors) {
+      setErrors(errors.errors);
     }
+  }
 
-    return (
-        <main>
-            <section className=" container mx-auto ">
-                <div>
-                    <p className="mb-2">Contact us</p>
-                    <p className="text-4xl font-semibold">Let’s Build Something</p>
-                    <p className="mt-3 text-lg dark:text-gray-300 text-gray-700">
-                        Don’t be shy, say hello! Ready to change the world with your project
-                        or have questions
-                    </p>
+  return (
+    <main>
+      <section className=" container mx-auto ">
+        <div>
+          <p className="mb-2">Contact us</p>
+          <p className="text-4xl font-semibold">Let’s Build Something</p>
+          <p className="mt-3 text-lg dark:text-gray-300 text-gray-700">
+            Don’t be shy, say hello! Ready to change the world with your project
+            or have questions
+          </p>
+        </div>
+        <div className={`py-20`}>
+          <div className="grid lg:grid-cols-5 gap-10">
+            <div className={`col-span-3`}>
+              <div className="mb-5">
+                <p className="mb-2 text-lg font-semibold">Contact Us</p>
+                <div className="flex justify-between">
+                  <p>info@katalyst.tech</p>
                 </div>
-                <div className={`py-20`}>
-                    <div className="grid lg:grid-cols-5 gap-10">
-                        <div className={`col-span-3`}>
-                            <div className="mb-5">
-                                <p className="mb-2 text-lg font-semibold">Contact Us</p>
-                                <div className="flex justify-between">
-                                    <p>info@katalyst.tech</p>
-                                </div>
-                            </div>
+              </div>
 
-                            <hr className=""/>
+              <hr className="" />
 
-                            <div className="mt-5">
-                                <p className="mb-4 text-lg font-semibold">Follow us</p>
-                                <div className="flex gap-14 text-2xl">
-                                    <BsDribbble/>
-                                    <BsLinkedin/>
-                                    <BsFacebook/>
-                                    <BsInstagram/>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={`col-span-2`}>
-                            <ContactForm handleSubmit={handleSubmit} errors={errors}/>
-
-                        </div>
-                    </div>
+              <div className="mt-5">
+                <p className="mb-4 text-lg font-semibold">Follow us</p>
+                <div className="flex gap-14 text-2xl">
+                  <BsDribbble />
+                  <BsLinkedin />
+                  <BsFacebook />
+                  <BsInstagram />
                 </div>
-            </section>
-
-
-        </main>
-    );
+              </div>
+            </div>
+            <div className={`col-span-2`}>
+              <ContactForm handleSubmit={handleSubmit} errors={errors} />
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
--- app/components/ui/ThemeToggle.jsx
+++ app/components/ui/ThemeToggle.jsx
@@ -13,19 +13,19 @@ import {
 } from './dropdown-menu';
 
 function ThemeToggle() {
-  const { setTheme } = useTheme('light');
+  const { setTheme } = useTheme();
 
   return (
-    <div className={`bg-red-500`}>
+    <div >
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="outline" size="icon">
-            <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 text-blue-500 transition-all dark:-rotate-90 dark:scale-0" />
             <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
             <span className="sr-only">Toggle theme</span>
           </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent className={`bg-red-500`} align="end">
+        <DropdownMenuContent  align="end">
           <DropdownMenuItem onClick={() => setTheme('light')}>
             Light
           </DropdownMenuItem>
