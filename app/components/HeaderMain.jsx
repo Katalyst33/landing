11e13dda8fa0656f0ecc2c475ba8780560c513aa
katalyst33 <>
2024-03-31 03:33:01 +0100

    add mobile menu

--- app/components/HeaderMain.jsx
+++ app/components/HeaderMain.jsx
@@ -1,97 +1,89 @@
-import { useState } from 'react';
-import { Dialog } from '@headlessui/react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import {useState} from 'react';
+import {Dialog} from '@headlessui/react';
+import {Bars3Icon, XMarkIcon} from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { AppIcon } from '../components/icons/AppIcon';
+import {AppIcon} from '../components/icons/AppIcon';
 import ThemeToggle from '../components/ui/ThemeToggle';
 import UserAuthData from '../../app/components/auth/User';
+import {usePathname} from "next/navigation";
+import {cn} from "../utils/cn";
+import {  Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,} from "../components/ui/sheet";
 
 const navigation = [
-  { name: 'Our Projects', href: '/project' },
-  { name: 'Our Services', href: '/services' },
-  { name: 'About Us', href: '/about' },
-  { name: 'Contact Us', href: '/contact' },
+    {name: 'Our Projects', href: '/project'},
+    {name: 'Our Services', href: '/services'},
+    {name: 'About Us', href: '/about'},
+    {name: 'Contact Us', href: '/contact'},
 ];
 
 export default function HeaderMain() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+    const pathname = usePathname()
 
-  return (
-    <header className=" border-b dark:border-gray-700 lg:px-32 px-4">
-      <nav
-        className="mx-auto flex max-w-7xl items-center justify-between py-4"
-        aria-label="Global"
-      >
-        <div className={`w-52`}>
-          <Link className="flex items-center " href="/">
-            <AppIcon name={true} className={`h-10 w-10`} />
-          </Link>
-        </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            onClick={() => setMobileMenuOpen(true)}
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
-        <div className="hidden lg:flex lg:gap-x-12">
-          {navigation.map((item) => (
-            <Link
-              key={item.name}
-              href={item.href}
-              className="text-sm font-semibold leading-6 "
+    return (
+        <header className=" border-b dark:border-gray-700 lg:px-32 px-4">
+            <nav
+                className="mx-auto flex max-w-7xl items-center justify-between py-4"
+                aria-label="Global"
             >
-              {item.name}
-            </Link>
-          ))}
-        </div>
-        <div className={`flex items-center gap-x-4`}>
-          <UserAuthData/>
+                <div className={`w-52`}>
+                    <Link className="flex items-center " href="/">
+                        <AppIcon name={true} className={`h-10 w-10`}/>
+                    </Link>
+                </div>
 
-          <ThemeToggle />
-        </div>
-      </nav>
-      <Dialog
-        as="div"
-        className="lg:hidden"
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}
-      >
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <Link href="/" className="-m-1.5 p-1.5 flex items-center">
-              <AppIcon name={true} className={`h-10 w-10`} />
-            </Link>
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5 dark:text-gray-300 text-gray-700"
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-            </div>
-          </div>
-        </Dialog.Panel>
-      </Dialog>
-    </header>
-  );
+                <div className="hidden lg:flex lg:gap-x-12">
+                    {navigation.map((item) => (
+                        <Link
+                            key={item.name}
+                            href={item.href}
+                            className="text-sm font-semibold leading-6 "
+                        >
+                            {item.name}
+                        </Link>
+                    ))}
+                </div>
+                <div className={`flex items-center gap-x-4`}>
+                    <div className="flex lg:hidden">
+                        <Sheet >
+                            <SheetTrigger>   <Bars3Icon className="h-6 w-6" aria-hidden="true"/></SheetTrigger>
+                            <SheetContent >
+                                <SheetHeader>
+                                    <Link href="/" className="-m-1.5 p-1.5 flex items-center">
+                                        <AppIcon name={true} className={`h-10 w-10`}/>
+                                    </Link>
+                                </SheetHeader>
+                                <div className={`mt-6`}>
+                                    {navigation.map((item) => (
+                                        <Link
+                                            key={item.name}
+                                            href={item.href}
+                                            className={cn(
+                                                '-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:bg-gray-200  dark:hover:bg-gray-700',
+                                                pathname === item.href ? 'bg-blue-400 hover:bg-blue-500 text-white' : 'text-gray-900 dark:text-white',
+                                                ' hover:font-bold'
+                                            )}
+                                        >
+                                            {item.name}
+                                        </Link>
+                                    ))}
+                                </div>
+                            </SheetContent>
+                        </Sheet>
+                    </div>
+
+                    <UserAuthData/>
+
+                    <ThemeToggle/>
+                </div>
+            </nav>
+        
+        </header>
+    );
 }
